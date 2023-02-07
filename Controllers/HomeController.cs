edc37d8dc20b02d008a31dba3aea6e777a565c2d
Osama Kamel <osama.w.kamel@gmail.com>
2023-02-07 03:36:03 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace westcoast_education.web.Controllers;
+
+public class HomeController : Controller
+{
+    //Action method...
+    public IActionResult Index()
+    {
+        ViewBag.Message = "Vi har Kurser för att plugga!";
+        //Returnerar ett ViewResult...
+        return View("Start");
+    }
+}
